d8e8962385dbf706f4807bc08f3b12c859388ff5
Rohan Yeldi <159413475+ROHAANYELDI@users.noreply.github.com>
2024-02-08 15:23:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.title('IRIS FLOWER DASHBOARD')
 with st.sidebar:
     st.header('Description')
     st.write('The Iris dataset was used in R.A. Fishers classic 1936 paper, The Use of Multiple Measurements in Taxonomic Problems, and can also be found on the UCI Machine Learning Repository. It includes three iris species with 50 samples each as well as some properties about each flower. One flower species is linearly separable from the other two, but the other two are not linearly separable from each other.')
-my_dataset = "C:/Users/User38/Desktop/Deployment/Iris.csv"
+my_dataset = "Iris.csv"
 df = pd.read_csv(my_dataset)
 df
 if st.checkbox("Preview DataFrame"):
